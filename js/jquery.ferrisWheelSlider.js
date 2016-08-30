b0852a2d206006507adba165d0aad7f22eb21e66
Hedgehog333 <artem1100.94@gmail.com>
2016-08-30 18:02:50 +0300

    add setting

--- js/jquery.ferrisWheelSlider.js
+++ js/jquery.ferrisWheelSlider.js
@@ -1,7 +1,17 @@
 (function( $ ) {
     $.fn.ferrisWheelSlider = function(option) {
-        var setting = {
+        var setting = $.extend({
+            arrows: true,
+            autoPlay: true,
+            duration: 5000,
+            radius: 600,
+
+        };
+
+        var make = function(){
             
         };
+
+        return this.each(make);
     };
 })(jQuery);
